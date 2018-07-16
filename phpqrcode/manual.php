8cb6e45c3b75a1187b0ffa37d73ca5aa4bb41081
pbutarbutar <paru.liano@gmail.com>
2018-07-16 14:19:54 +0700

    By Parul

---
+++ phpqrcode/manual.php
@@ -0,0 +1,2 @@
+<img src="http://localhost:8888/development/MyGithub/bosbuencrypt/phpqrcode/manual_1.php?data=121313535636363346121313535636a3346dasfsasfffsaafasfafaffsasfdasfasfaffasfsdadafasfsaafasfa948885812" width="180" />
+
